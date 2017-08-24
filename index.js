58d404b27480db506bef27d08750f60f1ffd496f
Dave Aronson <davearonson@users.noreply.github.com>
2017-08-23 20:52:18 -0400

    add write-to-log button

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const {app, Menu, Tray} = require('electron')
+const {app, Menu, MenuItem, Tray} = require('electron')
 
 let tray = null
+
+function write_to_log() { console.log('clicked the button') }
+
 app.on('ready', () => {
   tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
+    {label: 'Log', click: write_to_log},
     {label: 'Quit', role: 'quit'},
     {label: 'Item1', type: 'radio'},
     {label: 'Item2', type: 'radio'},
